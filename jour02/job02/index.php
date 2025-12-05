6916f66dd5d9b86033651ac612c4ba328df75ee7
Pauline-hiez <pauline.hiez@laplateforme.io>
2025-12-05 15:00:42 +0100

    jour02 - job02

---
+++ jour02/job02/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Apparition & Disparition</title>
+</head>
+
+<body>
+
+    <button onclick="showhide()" id="button">Cliquez</button>
+    <article id="text">L'important n'est pas la chute, mais l'atterrissage.</article>
+
+    <script src="./script.js"></script>
+</body>
+
+</html>
