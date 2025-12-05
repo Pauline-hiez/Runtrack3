7b233ebcde07fbb4e7ce2cd6af290aac2bc5948e
Pauline-hiez <pauline.hiez@laplateforme.io>
2025-12-05 15:36:50 +0100

    jour02 - job03

---
+++ jour02/job03/script.js
@@ -0,0 +1,8 @@
+let compteur = 0;
+
+function addone() {
+    compteur++;
+    document.getElementById("compteur").textContent = compteur;
+}
+
+document.getElementById("button").addEventListener("click", addone);
