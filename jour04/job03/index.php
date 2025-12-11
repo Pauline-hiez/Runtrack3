7a105ea22adc0f26f8144f848626520df118c00c
Pauline-hiez <pauline.hiez@laplateforme.io>
2025-12-11 10:50:50 +0100

    jour04 - job03 style en cours

--- jour04/job03/index.php
+++ jour04/job03/index.php
@@ -5,27 +5,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Job 03</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
 
-    <form id="filterForm">
+    <form class="form" id="filterForm">
         <input name="id" type="text" placeholder="ID">
         <input name="nom" type="text" placeholder="Nom">
         <select name="type">
             <option value="">Type</option>
         </select>
-        <input name="filtrer" type="button" value="Filtrer">
+        <input class="btn" name="filtrer" type="button" value="Filtrer">
     </form>
     <div class="results" id="results"></div>
 
-    <style>
-        input,
-        select {
-            border-radius: 10px;
-            padding: 5px;
-        }
-    </style>
 
     <script src="./script.js"></script>
 </body>
