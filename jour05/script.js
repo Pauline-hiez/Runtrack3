eb7904e9f15028c9c5bdd4437731fca2b946287b
Pauline-hiez <pauline.hiez@laplateforme.io>
2025-12-12 09:31:15 +0100

    Jour05 - Formulaires et style ok

---
+++ jour05/script.js
@@ -0,0 +1,8 @@
+//Navbar
+const toggle = document.getElementById("toggle");
+const nav = document.getElementById("nav");
+
+toggle.addEventListener("click", () => {
+    nav.classList.toggle("active");
+});
+
