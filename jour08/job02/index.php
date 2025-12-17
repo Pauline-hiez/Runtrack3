bbe10cefccb4b47103cb97aaa847cec24f14ed10
Pauline-hiez <pauline.hiez@laplateforme.io>
2025-12-17 13:21:18 +0100

    jour08 - job03

--- jour08/job02/index.php
+++ jour08/job02/index.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Job 01</title>
+    <title>Job 02</title>
     <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
 </head>
 
